beea409056d1090b7d3db416098514c266b57629
Artiom_Sokolov <sokolova-net@mail.ru>
2022-07-28 16:31:19 +0300

    connect Tailwind for styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div>
-      Heeloo
-    </div>
-  );
+  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
 }
 
 export default App;
